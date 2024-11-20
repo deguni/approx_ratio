bbf61903e3603ae18c6005be87dacf21149dc509
deguni <dymaksimov@list.ru>
2024-11-20 13:11:05 +0300

    First commit.

---
+++ README.md
@@ -0,0 +1,36 @@
+# Approx_ratio
+> A very simple program
+
+Find a fraction from decimal representation.
+
+## Installing / Getting started
+
+No installation supposed.
+
+## Developing
+
+UI development is welcome.
+
+### Building
+
+CMake build system used.
+
+## Features
+
+Input is hard-coded.
+
+## Contributing
+
+Comments are warmly welcome.
+
+<!---
+## Links
+
+- Project homepage: 
+- Repository: 
+- Related projects:
+-->
+
+## Licensing
+
+The code in this project is licensed under BSD 3-Clause license.
---
+++ approx_ratio.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+int main()
+{
+    //double a = 9.71951800E-03;
+    //double a = 9.72398370E-03;
+    //double a = 9.72398370E-04;
+    double   a = 9.72399176E-04 * 1000;
+    double err = std::numeric_limits<double>::max();
+    std::cout << "Fraction for " << a << ":\n";
+    for (int i = 1; i < 10000; i++) {
+        int j_ = -1;
+        for (int j = 0; j < i; j++) {
+            double curerr = fabs(1. * j / i - a);
+            if (curerr < err) {
+                j_ = j;
+                err = curerr;
+            }
+        }
+        if (j_ != -1) {
+            std::cout << j_ << "/" << i << " err=" << err << "\n";
+        }
+    }
+}
